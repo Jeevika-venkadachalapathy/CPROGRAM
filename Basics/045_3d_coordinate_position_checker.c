667c3942761eaf218aecc5d5828ef53389d12d0f
Jeevika V <vpjeevika@gmail.com>
2026-03-03 15:41:31 +0530

    Add files via upload

---
+++ Basics/045_3d_coordinate_position_checker.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int x,y,z;
+    scanf("%d %d %d",&x,&y,&z);
+    if(x==0&&y==0&&z==0)
+    printf("origin");
+    else if(x==0&&y==0)
+    printf("z-axis");
+    else if(x==0&&z==0)
+    printf("y-axis");
+    else if(y==0&&z==0)
+    printf("x-axis");
+    else if(x==0)
+    printf("yz-plane");
+    else
+    printf("3D space");
+}
