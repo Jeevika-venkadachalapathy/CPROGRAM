e662066459f15c08149181992922035727f4fe33
Jeevika V <vpjeevika@gmail.com>
2026-03-28 22:16:54 +0530

    Add files via upload

---
+++ Basics/049_lion_age.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+    int age;
+    printf("Enter lion age: ");
+    scanf("%d", &age);
+    if (age >= 0 && age <= 2)
+        printf("Cub");
+    else if (age <= 6)
+        printf("Juvenile");
+    else if (age <= 10)
+        printf("Subadult");
+    else if (age <= 15)
+        printf("Young Adult");
+    else if (age <= 26)
+        printf("Old Adult");
+    else
+        printf("Invalid Age");
+
+    return 0;
+}
